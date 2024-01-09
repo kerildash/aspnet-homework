31d19d15c9037d44334b246c0e861c884d0dedb1
kerildash <kerildash@gmail.com>
2024-01-09 21:18:14 +0300

    Add a class to do some things with arrays.

---
+++ Logic/ArrayHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Logic
+{
+    public class ArrayHandler
+    {
+        public double OddIndexedElementsSum(double[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("Array is Null");
+            }
+            if (array.Length <= 1) // no elements => no odd-indexed elements
+                                   // 1 element is 0st => no odd-indexed elements
+            {
+                throw new ArgumentException("No odd-indexed elements");
+            }
+
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
