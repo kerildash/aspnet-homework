796a15fe44744f43d8374f5176f9fdcf3bb31000
kerildash <kerildash@gmail.com>
2024-01-10 17:11:21 +0300

    Demonstrate initial variant of code in action

--- OcpAndDecoratorExample/Program.cs
+++ OcpAndDecoratorExample/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace OcpAndDecoratorExample
+﻿using OcpAndDecoratorExample.InitialPoint;
+
+namespace OcpAndDecoratorExample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            var menu = new Menu(new Robot());
+            menu.PrintMenu();
         }
     }
 }
