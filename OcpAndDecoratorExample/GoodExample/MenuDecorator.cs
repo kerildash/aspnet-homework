31174f382e67f607c73b1c443e421549304c947d
kerildash <kerildash@gmail.com>
2024-01-10 19:39:37 +0300

    Add the MenuDecorator to implement Decorator pattern and avoid violation of OCP.

---
+++ OcpAndDecoratorExample/GoodExample/MenuDecorator.cs
@@ -0,0 +1,28 @@
+﻿
+namespace OcpAndDecoratorExample.GoodExample;
+
+
+public abstract class MenuDecorator : IMenu
+{
+    public IMenu Menu;
+    public Robot Robot;
+    
+
+    internal MenuDecorator(IMenu menu, Robot robot) 
+    {
+        Menu = menu;
+        Robot = robot;
+    }
+    public virtual void HandleInput(string menuItem)
+    {
+        Menu.HandleInput(menuItem);
+    }
+    public virtual List<string> GenerateMenuView()
+    {
+        return Menu.GenerateMenuView();
+    }
+    public virtual string PrintMenu(List<string> view)
+    {
+        return Menu.PrintMenu(view);
+    }
+}
