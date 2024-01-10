8b7dfc01c7bad62495b69ca1ad8cb4cfe5e6f10f
kerildash <kerildash@gmail.com>
2024-01-10 19:34:43 +0300

    Add the interface of a menu.

---
+++ OcpAndDecoratorExample/GoodExample/IMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcpAndDecoratorExample.GoodExample;
+
+public interface IMenu
+{
+    public string PrintMenu(List<string> view);
+    public List<string> GenerateMenuView();
+    public void HandleInput(string menuItem);
+}
