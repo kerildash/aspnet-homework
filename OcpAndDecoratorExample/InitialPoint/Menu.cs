c5e21a6b534d2005a216f6db460b88e351b5ff9d
kerildash <kerildash@gmail.com>
2024-01-10 17:10:18 +0300

    Add the example of code at the initial point of time (before changing it)

---
+++ OcpAndDecoratorExample/InitialPoint/Menu.cs
@@ -0,0 +1,65 @@
+﻿
+// The code at the moment before making changes in it
+namespace OcpAndDecoratorExample.InitialPoint;
+
+
+//Menu class to provide a cli for program user
+internal class Menu
+{
+    Robot Robot;
+    internal Menu(Robot robot)
+    {
+        Robot = new Robot();
+    }
+    
+    internal virtual void PrintMenu()
+    {
+        var view = GenerateMenuView();
+        foreach (var item in view)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    private protected List<string> GenerateMenuView()
+    {
+        List<string> optionLIst =
+        [
+            "\n   MENU",
+            "1. Do first job.",
+            "2. Do second job.",
+            "",
+            "0. Exit.",
+            "",
+            "Choose what to do: ",
+        ];
+        return optionLIst;
+    }
+
+    internal void HandleInput()
+    {
+        try
+        {
+            string menuItem = Console.ReadLine().Trim();
+            Console.WriteLine("");
+
+            switch (menuItem)
+            {
+                case "1":
+                    Robot.DoWork1();
+                    break;
+                case "2":
+                    Robot.DoWork2();
+                    break;
+                case "0":
+                    break;
+                default:
+                    Console.WriteLine("Incorrect input");
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ OcpAndDecoratorExample/Robot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcpAndDecoratorExample
+{
+    internal class Robot
+    {
+        internal void DoWork1()
+        {
+            Console.WriteLine("Do first work");
+        }
+        internal void DoWork2()
+        {
+            Console.WriteLine("Do second work"); 
+        }
+        internal void DoWork3() 
+        { 
+            Console.WriteLine("Do third work"); 
+        }
+        internal void DoWork4() 
+        {
+            Console.WriteLine("Do fourth work"); 
+        }
+    }
+}
+
